9f00706603f302eb29e803108bf135a5efe88634
darun000 <reachoutdarun@gmail.com>
2026-02-17 11:10:09 +0530

    UC1 completed - Welcome message implemented

---
+++ src/UseCase1PalindromeCheckerApp.java
@@ -0,0 +1,18 @@
+public class UseCase1PalindromeCheckerApp {public static void main(String[] args) {
+
+    // Application Name
+    String appName = "Palindrome Checker App";
+
+    // Application Version
+    String version = "Version 1.0";
+
+    // Welcome Message
+    System.out.println("================================");
+    System.out.println("Welcome to " + appName);
+    System.out.println(version);
+    System.out.println("================================");
+    System.out.println("This application checks whether a given string is a palindrome.");
+    System.out.println();
+}
+
+}
